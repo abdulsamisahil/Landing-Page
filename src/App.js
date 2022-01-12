8d9f84764c826068a3fc70fbbe771b185d7fa17e
Abdul Sami <sami.kdr@live.se>
2022-01-12 18:05:00 +0100

    shop.js fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
+import Shop from './components/Shop';
 function App() {
   return (
     <div className="App">
       <Header/>
       <About/>
+      <Shop/>
     </div>
   );
 }
---
+++ src/components/Shop.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div className='shop'>
+            <div className="container">
+                <div className="shop__text">
+                    <div>
+                        <h1>Free Delivery With PIZZA OF THE DAY Only 
+                            <h3>59.99 SEK</h3>
+                        </h1>
+                        <div className="shop__btn">
+                            <button className="btn">SHOP NOW</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Shop
